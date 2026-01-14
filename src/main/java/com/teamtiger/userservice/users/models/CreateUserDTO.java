90d61a4715a9f93ff9c28c4430031f445104d421
Dan Jackson <danieljackson634@gmail.com>
2026-01-14 20:04:36 +0000

    Added unit test to get registering a user

--- src/main/java/com/teamtiger/userservice/users/models/CreateUserDTO.java
+++ src/main/java/com/teamtiger/userservice/users/models/CreateUserDTO.java
@@ -4,9 +4,11 @@ import com.teamtiger.userservice.users.UserConstants;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class CreateUserDTO {
 
     @NotBlank
---
+++ src/test/java/com/teamtiger/userservice/services/UserServiceTest.java
@@ -0,0 +1,36 @@
+package com.teamtiger.userservice.services;
+
+import com.teamtiger.userservice.users.models.CreateUserDTO;
+import com.teamtiger.userservice.users.models.CreatedUserDTO;
+import com.teamtiger.userservice.users.services.UserService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest(properties = "jwt.secret=bm90LXRoZS1yZWFsLXNlY3JldC1idXQtbWFrZS1pdC1sb25nLWVub3VnaC0yNTYtYml0cw==")
+@Transactional
+public class UserServiceTest {
+
+    @Autowired
+    private UserService userService;
+
+    @Test
+    public void testRegisterUser() {
+        CreateUserDTO createUserDTO = CreateUserDTO.builder()
+                .email("example@exeter.ac.uk")
+                .username("example1")
+                .password("password1")
+                .build();
+
+        CreatedUserDTO createdUserDTO = userService.createUser(createUserDTO);
+        Assertions.assertFalse(createdUserDTO.getRefreshToken().isEmpty());
+
+    }
+
+
+}
