69a89c1cfe72055c8c6e02f742a70ce0dd2ed435
Dan Jackson <danieljackson634@gmail.com>
2026-01-16 19:34:45 +0000

    Automatic CD Deployment Test

--- src/main/java/com/teamtiger/userservice/vendors/controllers/VendorController.java
+++ src/main/java/com/teamtiger/userservice/vendors/controllers/VendorController.java
@@ -1,7 +1,9 @@
 package com.teamtiger.userservice.vendors.controllers;
 
 import com.teamtiger.userservice.vendors.services.VendorService;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class VendorController {
 
     private final VendorService vendorService;
+
+    @GetMapping
+    public String test() {
+        return "Hello World";
+    }
 
 
 
