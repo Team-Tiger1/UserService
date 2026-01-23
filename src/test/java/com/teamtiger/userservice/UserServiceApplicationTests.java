773592f3a10473d74a4de4c3a941827698270d5f
JedLeas <jedleasyt@gmail.com>
2026-01-23 22:56:05 +0000

    fixed decloration of Test JWT_secret to capitilization

--- src/test/java/com/teamtiger/userservice/UserServiceApplicationTests.java
+++ src/test/java/com/teamtiger/userservice/UserServiceApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
-@SpringBootTest(properties = "jwt_secret=bm90LXRoZS1yZWFsLXNlY3JldC1idXQtbWFrZS1pdC1sb25nLWVub3VnaC0yNTYtYml0cw==")
+@SpringBootTest(properties = "JWT_SECRET=bm90LXRoZS1yZWFsLXNlY3JldC1idXQtbWFrZS1pdC1sb25nLWVub3VnaC0yNTYtYml0cw==")
 @Transactional
 class UserServiceApplicationTests {
 
