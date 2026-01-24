f34f7e0856298ad73aa15480aa3e6d1f4bf79a2f
Dan Jackson <danieljackson634@gmail.com>
2026-01-24 15:06:31 +0000

    Updated secret for unit tests

--- src/test/java/com/teamtiger/userservice/UserServiceApplicationTests.java
+++ src/test/java/com/teamtiger/userservice/UserServiceApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
-@SpringBootTest(properties = "JWT_SECRET=bm90LXRoZS1yZWFsLXNlY3JldC1idXQtbWFrZS1pdC1sb25nLWVub3VnaC0yNTYtYml0cw==")
+@SpringBootTest(properties = "jwt.secret=bm90LXRoZS1yZWFsLXNlY3JldC1idXQtbWFrZS1pdC1sb25nLWVub3VnaC0yNTYtYml0cw==")
 @Transactional
 class UserServiceApplicationTests {
 
