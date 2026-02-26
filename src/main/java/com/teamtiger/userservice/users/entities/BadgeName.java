b964dc2c9b16ba9ae240f9709fa741825e014fc3
Dan Jackson <danieljackson634@gmail.com>
2026-02-26 15:12:33 +0000

    ID-222 Added Waste King Badge Type

--- src/main/java/com/teamtiger/userservice/users/entities/BadgeName.java
+++ src/main/java/com/teamtiger/userservice/users/entities/BadgeName.java
@@ -5,6 +5,7 @@ public enum BadgeName {
     UNIQUE_SHOPPER, //Bundles from different vendors
     LOYAL_SHOPPER, //Bundles from same vendor
     HOT_SHOPPER, //Total amount of bundles
+    WASTE_KING,
 
 
 }
