ee872b3d2328ae9ade1a7639a55387bfe0119968
Dan Jackson <danieljackson634@gmail.com>
2026-03-05 17:38:17 +0000

    ID-222 Updated SQL Queries to be Native

--- src/main/java/com/teamtiger/userservice/users/repositories/StreakRepository.java
+++ src/main/java/com/teamtiger/userservice/users/repositories/StreakRepository.java
@@ -1,14 +1,14 @@
 package com.teamtiger.userservice.users.repositories;
 
 import com.teamtiger.userservice.users.entities.Streak;
-import org.hibernate.annotations.processing.SQL;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.UUID;
 
 public interface StreakRepository extends JpaRepository<Streak, UUID> {
 
-    @SQL("SELECT streak FROM streaks where user_id = :userId")
+    @Query("SELECT streak FROM Streak where userId = :userId")
     Integer getStreakForUser(UUID userId);
 
 }
