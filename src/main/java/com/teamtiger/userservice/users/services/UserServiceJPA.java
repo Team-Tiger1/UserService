20c45bb3f14b68fa920fcc6056f9248eb547cf65
Dan Jackson <danieljackson634@gmail.com>
2026-03-08 14:31:54 +0000

    ID-222 Added Transactional To Badge Read Method

--- src/main/java/com/teamtiger/userservice/users/services/UserServiceJPA.java
+++ src/main/java/com/teamtiger/userservice/users/services/UserServiceJPA.java
@@ -285,6 +285,7 @@ public class UserServiceJPA implements UserService {
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<UserBadgeDTO> getAllBadgesForUser(String accessToken) {
         String role = jwtTokenUtil.getRoleFromToken(accessToken);
 
