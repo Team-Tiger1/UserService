e133f2acc06a1f103d0b3647bb16c6509630dcee
Dan Jackson <danieljackson634@gmail.com>
2026-03-09 18:58:53 +0000

    ID-219 Added Constructors to CreateDisputeDTO

--- src/main/java/com/teamtiger/userservice/users/models/CreateDisputeDTO.java
+++ src/main/java/com/teamtiger/userservice/users/models/CreateDisputeDTO.java
@@ -5,14 +5,18 @@ import com.teamtiger.userservice.users.entities.disputes.DisputeStatus;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
 
 import java.util.UUID;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CreateDisputeDTO {
 
     @NotNull
