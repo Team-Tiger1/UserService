bd330bdf65c82e37bb928a06ffe083b009e9d936
Dan Jackson <danieljackson634@gmail.com>
2026-03-17 14:35:18 +0000

    ID-222 Updated Waste Badge Thresholds to be in grams

--- src/main/java/com/teamtiger/userservice/users/entities/badges/BadgeValues.java
+++ src/main/java/com/teamtiger/userservice/users/entities/badges/BadgeValues.java
@@ -8,7 +8,7 @@ public class BadgeValues {
             BadgeName.THE_EXPLORER, new Double[]{3.0, 5.0, 10.0},
             BadgeName.LOYAL_SHOPPER, new Double[]{5.0, 10.0, 20.0},
             BadgeName.HOT_SHOPPER, new Double[]{5.0, 15.0, 25.0},
-            BadgeName.WASTE_KING, new Double[]{5.0, 25.0, 100.0},
+            BadgeName.WASTE_KING, new Double[]{5000.0, 10000.0, 20000.0},
             BadgeName.CATEGORY_KING, new Double[]{2.0, 4.0, 6.0},
             BadgeName.WEEKLY_WARRIOR, new Double[]{2.0, 4.0, 8.0},
             BadgeName.WALLET_WATCHER, new Double[]{10.0, 50.0, 100.0}
